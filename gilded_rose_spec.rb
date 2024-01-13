13ed80b78376c6c712f23e5d8867dc8a4ef5fa9b
VictorPS <victorpsilvano@gmail.com>
2024-01-13 18:12:46 +0000

    test: Add a test based on the specifications ...
    This test was slightly different from the specification because the item name needs to match
    "Sulfuras, Hand of Ragnaros"
    instead of
    "Sulfuras"

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -31,5 +31,12 @@ describe GildedRose do
       GildedRose.new(items).update_quality
       expect(items[0].quality).to eq 50
     end
+
+    it "`Sulfuras, Hand of Ragnaros`, being a legendary item, \
+        never has to be sold or decreases in `Quality`" do
+      items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 50)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 50
+    end
   end
 end
