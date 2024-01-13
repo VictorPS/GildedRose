1d9b3276a5b9553b47db5c378b5c86abf91c39b5
VictorPS <victorpsilvano@gmail.com>
2024-01-13 23:03:24 +0000

    doc: Add some comments giving more information about the kata

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,10 +1,13 @@
+# The idea of this Kata is that we are working with a legacy system and there
+# are a few pieces of code that we shouldn't modify.
+# In this case, we are not allowed to change the arguments of the initialize
+# and update_quality methods.
 class GildedRose
-
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
@@ -53,6 +56,10 @@ class GildedRose
   end
 end
 
+##############################
+# THIS BELONGS TO THE GOBLIN #
+#      DO NOT CHANGE IT      #
+##############################
 class Item
   attr_accessor :name, :sell_in, :quality
 
@@ -62,7 +69,7 @@ class Item
     @quality = quality
   end
 
-  def to_s()
+  def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
