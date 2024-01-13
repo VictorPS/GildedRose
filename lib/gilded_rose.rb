380d54e4fcfb88dcd3ec451d612c1e149d0e2130
VictorPS <victorpsilvano@gmail.com>
2024-01-13 23:16:43 +0000

    refactor: Remove any redundant condition for Sulfuras

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -17,9 +17,7 @@ class GildedRose
 
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
+          item.quality = item.quality - 1
         end
       else
         if item.quality < 50
@@ -38,16 +36,12 @@ class GildedRose
           end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
+      item.sell_in = item.sell_in - 1
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
+              item.quality = item.quality - 1
             end
           else
             item.quality = item.quality - item.quality
