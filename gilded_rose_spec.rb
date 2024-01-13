2163f240c3b86254d57e33999afc61c69b1c1580
VictorPS <victorpsilvano@gmail.com>
2024-01-13 18:04:22 +0000

    test: Add a test based on the specifications

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -7,5 +7,11 @@ describe GildedRose do
       GildedRose.new(items).update_quality
       expect(items[0].quality).to eq 8
     end
+
+    it "The `Quality` of an item is never negative" do
+      items = [Item.new("Common item", 0, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 0
+    end
   end
 end
