59c72ecdeea5453789fd487a25d85170fc1ab489
VictorPS <victorpsilvano@gmail.com>
2024-01-13 17:55:11 +0000

    test: Add the first test based on the specifications
    The idea of this Kata is to start implementing the tests until you feel comfortable with their coverage.
    Once you feel that your tests are covering all the cases you can start refactoring.

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -1,13 +1,11 @@
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
-
   describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+    it "Once the sell by date has passed, `Quality` degrades twice as fast" do
+      items = [Item.new("Common item", 0, 10)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 8
     end
   end
-
 end
