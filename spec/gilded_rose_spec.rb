ca3b0234b31430f4ab595c80960ddd59aecadfd1
VictorPS <victorpsilvano@gmail.com>
2024-01-21 19:05:59 +0000

    refactor/test: Grouped Common items tests and add a test for when the sell by date is positive

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,22 +2,30 @@ require_relative "../lib/gilded_rose"
 
 describe GildedRose do
   describe "#update_quality" do
-    it "Once the sell by date has passed, `Quality` degrades twice as fast" do
-      items = [Item.new("Common item", 0, 10)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 8
-    end
+    describe "Common item" do
+      it "Before the sell by date has passed, `Quality` degrades by 1" do
+        items = [Item.new("Common item", 1, 10)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq 9
+      end
 
-    it "The `Quality` of an item is never negative" do
-      items = [Item.new("Common item", 0, 0)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 0
-    end
+      it "Once the sell by date has passed, `Quality` degrades by 2" do
+        items = [Item.new("Common item", 0, 10)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq 8
+      end
 
-    it "Items sell by date can be negative" do
-      items = [Item.new("Common item", 0, 0)]
-      GildedRose.new(items).update_quality
-      expect(items[0].sell_in).to eq -1
+      it "The `Quality` of an item is never negative" do
+        items = [Item.new("Common item", 0, 0)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq 0
+      end
+
+      it "Items sell by date can be negative" do
+        items = [Item.new("Common item", 0, 0)]
+        GildedRose.new(items).update_quality
+        expect(items[0].sell_in).to eq -1
+      end
     end
 
     it "`Aged Brie` actually increases in `Quality` the older it gets" do
