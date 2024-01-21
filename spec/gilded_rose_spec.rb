b3b726b7b05beb3c258219713c9822f9b70d5ed9
VictorPS <victorpsilvano@gmail.com>
2024-01-21 19:21:12 +0000

    refactor: Grouped the Backstage passes tests and improve the wording

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -63,32 +63,30 @@ describe GildedRose do
       end
     end
 
-    it "`Backstage passes to a TAFKAL80ETC concert` increases in `Quality` \
-        the older it gets" do
-      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 20, 0)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 1
-    end
+    describe "Backstage passes" do
+      it "Increases in `Quality` by 1 when the sell by date is greater than 10" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 12, 0)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq 1
+      end
 
-    it "`Backstage passes to a TAFKAL80ETC concert` increases in `Quality` \
-        by `2` after the 10th day the older it gets" do
-      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 0)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 2
-    end
+      it "Increases in `Quality` by 2 after the 10th day the older it gets" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 0)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq 2
+      end
 
-    it "`Backstage passes to a TAFKAL80ETC concert` increases in `Quality` \
-        by `3` after the 5th day the older it gets" do
-      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 0)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 3
-    end
+      it "Increases in `Quality` by 3 after the 5th day the older it gets" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 0)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq 3
+      end
 
-    it "`Backstage passes to a TAFKAL80ETC concert` `Quality` drops to `0` \
-        after the sell by date" do
-      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 10)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 0
+      it "`Quality` drops to `0` after the sell by date" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 10)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq 0
+      end
     end
 
     xit "`Conjured` items degrade in `Quality` twice as fast as normal items" do; end
