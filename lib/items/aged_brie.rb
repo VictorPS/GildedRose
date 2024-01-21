1cc34ef8054d14b7d674d53821c5bb3c78b934c7
VictorPS <victorpsilvano@gmail.com>
2024-01-21 19:35:11 +0000

    refactor: Make the Aged Brie logic more explicit

--- lib/items/aged_brie.rb
+++ lib/items/aged_brie.rb
@@ -5,16 +5,10 @@ module Items
     end
 
     def update_quality
-      @item.sell_in = @item.sell_in - 1
+      @item.sell_in -= 1
 
-      if @item.quality < 50
-        @item.quality = @item.quality + 1
-      end
-      if @item.sell_in < 0
-        if @item.quality < 50
-          @item.quality = @item.quality + 1
-        end
-      end
+      @item.quality += 1 if @item.quality < 50 && @item.sell_in >= 0
+      @item.quality += 2 if @item.quality < 50 && @item.sell_in < 0
     end
   end
 end
