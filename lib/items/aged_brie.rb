1dca7748027aa732216046f380cba768cb8ae584
VictorPS <victorpsilvano@gmail.com>
2024-01-21 19:50:41 +0000

    refactor: Introducing superior and inferior limits

--- lib/items/aged_brie.rb
+++ lib/items/aged_brie.rb
@@ -7,8 +7,9 @@ module Items
     def update_quality
       @item.sell_in -= 1
 
-      @item.quality += 1 if @item.quality < 50 && @item.sell_in >= 0
-      @item.quality += 2 if @item.quality < 50 && @item.sell_in < 0
+      @item.quality += 1 if @item.sell_in >= 0
+      @item.quality += 2 if @item.sell_in < 0
+      @item.quality = 50 if @item.quality > 50
     end
   end
 end
--- lib/items/backstage_passes.rb
+++ lib/items/backstage_passes.rb
@@ -7,10 +7,11 @@ module Items
     def update_quality
       @item.sell_in -= 1
 
-      @item.quality += 1 if @item.quality < 50
-      @item.quality += 1 if @item.sell_in < 10 && @item.quality < 50
-      @item.quality += 1 if @item.sell_in < 5 && @item.quality < 50
+      @item.quality += 1
+      @item.quality += 1 if @item.sell_in < 10
+      @item.quality += 1 if @item.sell_in < 5
       @item.quality = 0  if @item.sell_in < 0
+      @item.quality = 50 if @item.quality > 50
     end
   end
 end
