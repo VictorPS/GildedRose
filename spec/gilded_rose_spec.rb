a0b0825c13f379ae87f4dbae5856cc44142d1535
VictorPS <victorpsilvano@gmail.com>
2024-01-21 19:11:13 +0000

    refactor: Group Sulfuras tests
    The quality should be 80 as default but not enforced in the update.
    The sell_in also does not change when updating, so I decided to add an expectation for it.

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -54,11 +54,13 @@ describe GildedRose do
       end
     end
 
-    it "`Sulfuras, Hand of Ragnaros`, being a legendary item, \
-        never has to be sold or decreases in `Quality`" do
-      items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 50)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 50
+    describe "Sulfuras, Hand of Ragnaros" do
+      it "Never has to be sold or decreases in `Quality`" do
+        items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 80)]
+        GildedRose.new(items).update_quality
+        expect(items[0].sell_in).to eq 0
+        expect(items[0].quality).to eq 80
+      end
     end
 
     it "`Backstage passes to a TAFKAL80ETC concert` increases in `Quality` \
