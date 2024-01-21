2f837c3f2cf17bda3b31094ca29a3cd49889f47d
VictorPS <victorpsilvano@gmail.com>
2024-01-21 19:41:00 +0000

    refactor: Make the Backstage passes logic more explicit

--- lib/items/backstage_passes.rb
+++ lib/items/backstage_passes.rb
@@ -5,25 +5,12 @@ module Items
     end
 
     def update_quality
-      if @item.quality < 50
-        @item.quality = @item.quality + 1
-        if @item.sell_in < 11
-          if @item.quality < 50
-            @item.quality = @item.quality + 1
-          end
-        end
-        if @item.sell_in < 6
-          if @item.quality < 50
-            @item.quality = @item.quality + 1
-          end
-        end
-      end
+      @item.sell_in -= 1
 
-      @item.sell_in = @item.sell_in - 1
-
-      if @item.sell_in < 0
-        @item.quality = @item.quality - @item.quality
-      end
+      @item.quality += 1 if @item.quality < 50
+      @item.quality += 1 if @item.sell_in < 10 && @item.quality < 50
+      @item.quality += 1 if @item.sell_in < 5 && @item.quality < 50
+      @item.quality = 0  if @item.sell_in < 0
     end
   end
 end
